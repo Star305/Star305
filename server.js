9c141c7e3439ec88df1a42ad689222fe99235837
Star305 <emmanuelokeh34@gmail.com>
2025-07-03 15:49:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const app = express();
 const fs = require('fs');
 const path = require('path');
 
+const PORT = 3000;
+const dataFile = path.join(__dirname, 'data.json');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname)); // Serve static files
-
-const dataFile = path.join(__dirname, 'data.json');
+app.use(express.static(__dirname));
 
 app.get('/api/groups', (req, res) => {
   fs.readFile(dataFile, (err, data) => {
@@ -20,14 +21,14 @@ app.get('/api/groups', (req, res) => {
 });
 
 app.post('/api/groups', (req, res) => {
-  const { groupName, groupLink } = req.body;
+  const { groupLink } = req.body;
 
   fs.readFile(dataFile, (err, data) => {
     if (err) {
       res.status(500).send({ message: 'Error reading data file' });
     } else {
       const jsonData = JSON.parse(data);
-      jsonData.groups.push({ name: groupName, link: groupLink, views: 0, joins: 0 });
+      jsonData.groups.push({ link: groupLink, views: 0, joins: 0 });
 
       fs.writeFile(dataFile, JSON.stringify(jsonData, null, 2), (err) => {
         if (err) {
@@ -40,6 +41,6 @@ app.post('/api/groups', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
