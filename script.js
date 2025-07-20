52b9396b9922c710aca4f063a7ef1dccc9f4dccf
Star305 <emmanuelokeh34@gmail.com>
2025-07-21 00:06:21 +0100

    Update and rename gc.js to script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+const quotes = [
+  "Believe in yourself and all that you are.",
+  "Push harder than yesterday if you want a different tomorrow.",
+  "Success is not final, failure is not fatal.",
+  "Small steps every day lead to big results.",
+  "Dream it. Wish it. Do it.",
+  "Don’t watch the clock; do what it does. Keep going.",
+  "You are capable of amazing things."
+  "Time files but remember you're the pilot."
+  "Delay does not mean denial."
+  "For more or website like this contact +2349060245012"
+];
+
+function generateQuote() {
+  const quoteBox = document.getElementById("quote");
+  const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+  quoteBox.textContent = randomQuote;
+}
+```
