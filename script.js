865402de6b0d69780ed8dd9c812acbf419b35ee4
Star305 <emmanuelokeh34@gmail.com>
2025-07-23 09:08:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const quotes = [
   "Believe in yourself and all that you are.",
   "Push harder than yesterday if you want a different tomorrow.",
@@ -5,9 +6,9 @@ const quotes = [
   "Small steps every day lead to big results.",
   "Dream it. Wish it. Do it.",
   "Don’t watch the clock; do what it does. Keep going.",
-  "You are capable of amazing things."
-  "Time files but remember you're the pilot."
-  "Delay does not mean denial."
+  "You are capable of amazing things.",
+  "Time flies but remember you're the pilot.",
+  "Delay does not mean denial.",
   "For more or website like this contact +2349060245012"
 ];
 
